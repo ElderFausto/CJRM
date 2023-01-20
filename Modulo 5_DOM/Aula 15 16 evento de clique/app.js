23ac99675749aa76da782fea86ff44bd04a63808
Elder <elderfavsto@gmail.com>
2023-01-20 18:30:24 -0300

    exercicios DOM

--- Modulo 5_DOM/Aula 15 16 evento de clique/app.js
+++ Modulo 5_DOM/Aula 15 16 evento de clique/app.js
@@ -4,12 +4,24 @@
 //   console.log('clicou no botao');
 // })
 
+const ul = document.querySelector('ul')
+
 const lis = document.querySelectorAll('li')
 
 lis.forEach(li => {
   li.addEventListener('click', event => {
     const clickedElement = event.target
 
-    clickedElement.style.textDecoration = 'line-through'
+    clickedElement.remove()
   })
+})
+
+
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+  const li = document.createElement('li')
+
+  li.textContent = `Novo item`
+  ul.prepend(li)
 })
